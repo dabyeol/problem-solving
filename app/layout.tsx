1c0b70883d4dd9c95368c85734a89e73c8f3e179
Joohyung Park <johy@dabyeol.com>
2023-12-11 17:17:17 +0900

    Fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Layout from '@/components/layout';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import 'react-loading-skeleton/dist/skeleton.css';
 import './globals.css';
 import './normalize.css';
@@ -12,6 +12,20 @@ export const metadata: Metadata = {
   description: '기본에 충실한 문제 풀이',
 };
 
+export const viewport: Viewport = {
+  themeColor: [
+    {
+      media: '(prefers-color-scheme: light)',
+      color: '#fff',
+    },
+    {
+      media: '(prefers-color-scheme: dark)',
+      color: '#222',
+    },
+  ],
+  viewportFit: 'cover',
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -19,6 +33,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
+      </head>
       <body>
         <Layout>{children}</Layout>
       </body>
